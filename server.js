8077c01f8afebb6d3ffc596d9776d878a0ba4b2f
Maverick Fernandez <maverick.fernandez8@gmail.com>
2022-07-25 13:31:42 -0400

    added variables and Mongo test connection

--- server.js
+++ server.js
@@ -3,3 +3,15 @@ const app = express()
 const cors = require(cors)
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+
+let db, 
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'star-wars-quotes',
+    collection
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log("connected to database")
+        db = client.db(dbName)
+        collection = db.collection('quotes')
+    })
