8dafee769261eb404b72f1f41032f0a48930e2ce
Maverick Fernandez <maverick.fernandez8@gmail.com>
2022-07-26 16:52:22 -0400

    fixing timeout

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ MongoClient.connect(dbConnectionString)
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended:true }))
 app.use(express.json())
 app.use(cors())
 
