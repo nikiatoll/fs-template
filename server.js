9614a23ee0a7c49df2f9f59bf44454e071f2b68b
Maverick Fernandez <maverick.fernandez8@gmail.com>
2022-07-26 16:39:51 -0400

    fixing timeout

--- server.js
+++ server.js
@@ -1,36 +1,38 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const { response } = require('express')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+const PORT = 8000
 
-let db, 
+let db,
     dbConnectionString = process.env.DB_STRING,
-    dbName = 'template',
+    dbName = 'sample_mflix',
     collection
 
 MongoClient.connect(dbConnectionString)
     .then(client => {
-        console.log("connected to database")
+        console.log(`Connected to Database`)
         db = client.db(dbName)
-        collection = db.collection('test')
+        collection = db.collection('movies')
     })
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
 
-app.get('/', async (req, res) => {
-    try{
-        res.render('index.ejs')
+app.get('/', async (request, response) => {
+    try {
+        response.render('index.ejs')
     } catch (error) {
-        response.status(500).send({message: error.message})
+        response.status(500).send({ message: error.message })
     }
 })
 
-app.listen(process.env.PORT || PORT, ()=> {
+
+//PORT = 8000
+app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is running on port`)
 })
