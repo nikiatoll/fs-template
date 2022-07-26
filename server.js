302e73c6a9ece71a09a1d1d49ce7b8723847a0cf
Maverick Fernandez <maverick.fernandez8@gmail.com>
2022-07-26 16:35:15 -0400

    fixing timeout

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ require('dotenv').config()
 
 let db, 
     dbConnectionString = process.env.DB_STRING,
-    dbName = 'star-wars-quotes',
+    dbName = 'template',
     collection
 
 MongoClient.connect(dbConnectionString)
     .then(client => {
         console.log("connected to database")
         db = client.db(dbName)
-        collection = db.collection('quotes')
+        collection = db.collection('test')
     })
 
 app.set('view engine', 'ejs')
